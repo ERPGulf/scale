aa514b36d963222b1b44b96f94af772b6d718997
Harshafiroz <harsha@htsqatar.com>
2024-05-25 12:21:25 +0300

    Update __init__.py with new changes

--- scale/__init__.py
+++ scale/__init__.py
@@ -4,13 +4,13 @@ __version__ = "0.0.1"
 
 
 
-# from erpnext.selling.page.point_of_sale import point_of_sale
-# from erpnext.stock import get_item_details
-# from scale.my_pos import list_item_details, list_items, list_price
+from erpnext.selling.page.point_of_sale import point_of_sale
+from erpnext.stock import get_item_details
+from scale.my_pos import list_item_details, list_items, list_price
 
 
-# point_of_sale.get_items = list_items
-# get_item_details.get_item_details = list_item_details
-# get_item_details.get_price_list_rate = list_price
+point_of_sale.get_items = list_items
+get_item_details.get_item_details = list_item_details
+get_item_details.get_price_list_rate = list_price
 
 
